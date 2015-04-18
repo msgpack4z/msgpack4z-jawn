b3297949daaa8dad2be7a8dc9a2893166a08616d
xuwei-k <6b656e6a69@gmail.com>
2015-04-18 09:40:40 +0900

    msgpack java 0.7.0-p8

--- project/Build.scala
+++ project/Build.scala
@@ -13,7 +13,8 @@ object build extends Build {
       ("org.spire-math" %% "jawn-ast" % "0.7.2") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.2") ::
       ("org.scalacheck" %% "scalacheck" % "1.12.2" % "test") ::
-      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.2" % "test") ::
+      ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.3" % "test").exclude("org.msgpack", "msgpack-core") ::
+      ("org.msgpack" % "msgpack-core" % "0.7.0-p8" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java06" % "0.1.0" % "test") ::
       ("com.github.xuwei-k" %% "msgpack4z-native" % "0.1.0" % "test") ::
       Nil
