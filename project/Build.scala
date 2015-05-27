7e88800e2ea91150ce5973013f4feb1f6c55e5af
xuwei-k <6b656e6a69@gmail.com>
2015-05-28 03:42:47 +0900

    exclude jmh

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,11 @@ object build extends Build {
   ).settings(
     name := msgpack4zJawnName,
     libraryDependencies ++= (
-      ("org.spire-math" %% "jawn-ast" % "0.8.0") ::
+      ("org.spire-math" %% "jawn-ast" % "0.8.0").excludeAll( // https://github.com/non/jawn/issues/33
+         ExclusionRule("org.openjdk.jmh", "jmh-core"),
+         ExclusionRule("org.openjdk.jmh", "jmh-generator-bytecode"),
+         ExclusionRule("org.openjdk.jmh", "jmh-generator-reflection")
+      ) ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.1.2") ::
       ("org.scalacheck" %% "scalacheck" % "1.12.2" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.1.3" % "test").exclude("org.msgpack", "msgpack-core") ::
