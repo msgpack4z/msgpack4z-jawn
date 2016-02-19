fa050f3d186af8dcc6ccde7a92ad5cb4057d9533
xuwei-k <6b656e6a69@gmail.com>
2016-02-20 05:36:01 +0900

    jawn-ast 0.8.4

--- project/Build.scala
+++ project/Build.scala
@@ -10,7 +10,7 @@ object build extends Build {
   ).settings(
     name := msgpack4zJawnName,
     libraryDependencies ++= (
-      ("org.spire-math" %% "jawn-ast" % "0.8.3") ::
+      ("org.spire-math" %% "jawn-ast" % "0.8.4") ::
       ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.1") ::
       ("org.scalacheck" %% "scalacheck" % "1.12.5" % "test") ::
       ("com.github.xuwei-k" % "msgpack4z-java07" % "0.2.0" % "test") ::
