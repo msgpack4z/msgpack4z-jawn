c66196074ecd253c555081db7021b5aed3786b43
xuwei-k <6b656e6a69@gmail.com>
2016-07-31 12:01:56 +0900

    sbt 0.13.12

--- project/Build.scala
+++ project/Build.scala
@@ -1,25 +1,6 @@
-import sbt._, Keys._
+object build {
 
-object build extends Build {
-
-  private val msgpack4zJawnName = "msgpack4z-jawn"
+  val msgpack4zJawnName = "msgpack4z-jawn"
   val modules = msgpack4zJawnName :: Nil
-
-  lazy val msgpack4z = Project("msgpack4z-jawn", file(".")).settings(
-    Common.settings: _*
-  ).settings(
-    name := msgpack4zJawnName,
-    libraryDependencies ++= (
-      ("org.spire-math" %% "jawn-ast" % "0.8.4") ::
-      ("com.github.xuwei-k" %% "msgpack4z-core" % "0.3.2") ::
-      ("org.scalacheck" %% "scalacheck" % "1.13.0" % "test") ::
-      ("com.github.xuwei-k" % "msgpack4z-java" % "0.3.2" % "test") ::
-      ("com.github.xuwei-k" % "msgpack4z-java06" % "0.2.0" % "test") ::
-      ("com.github.xuwei-k" %% "msgpack4z-native" % "0.3.0" % "test") ::
-      Nil
-    )
-  ).settings(
-    Sxr.subProjectSxr(Compile, "classes.sxr"): _*
-  )
 
 }
